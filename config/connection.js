9a685ee5cabbeab5adb2e0ba555e2a722a4a3705
Payam <payamasem@berkeley.edu>
2018-03-05 08:44:21 -0800

    fixed broken stuff

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,15 @@
 var mysql = require("mysql");
 var connection;
 
+//jawsDB
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
  connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "i7bnstejile8r7ic",
+  password: "pikx9gcu7imsnszf",
+  database: "w5kq58cgp3cjcm97"
 });
 };
 
