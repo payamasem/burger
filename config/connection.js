963c22b168db61e0814d19af4096edbb5c02a16a
Payam <payamasem@berkeley.edu>
2018-03-05 08:57:05 -0800

    sql working

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
  connection = mysql.createConnection({
-  host: "gp96xszpzlqupw4k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-  user: "i7bnstejile8r7ic",
-  password: "pikx9gcu7imsnszf",
-  database: "w5kq58cgp3cjcm97"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "burgers_db"
 });
 };
 
