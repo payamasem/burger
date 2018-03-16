a73358dc03ac2e4cc9d680641c7f0fba3b304946
Payam <payamasem@berkeley.edu>
2018-03-16 10:15:19 -0700

    fixed heroku issue

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Double Cheeseburger with Bacon');
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Black Bean Veggie Burger');
-INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Jalepeno Popper Burger');
+INSERT INTO `g0a71irsd2jlnh7`.`burgers` (`burger_name`) VALUES ('Double Cheeseburger with Bacon');
+INSERT INTO `g0a71irsd2jlnh7`.`burgers` (`burger_name`) VALUES ('Black Bean Veggie Burger');
+INSERT INTO `g0a71irsd2jlnh7`.`burgers` (`burger_name`) VALUES ('Jalepeno Popper Burger');
